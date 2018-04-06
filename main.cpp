fad1453d6a63cb21a0c78c1ba8ad879d477a3bac
pvang40 <pvang40@mail.csuchico.edu>
2018-04-06 15:38:33 -0700

    vector of vectors

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author: Pachia Vang 
 #include <iostream>
 #include <vector>
 
@@ -6,6 +7,7 @@ using namespace std;
 int main()
 {
   int letters;
+  vector <vector <char> > alphabet; 
 
   do
   {
@@ -17,6 +19,49 @@ int main()
       cout<<"\nPlease enter a number between 1-26.\n";
     }
   }while( letters < 1 || letters > 26 );
+  
+  int row ;
+  if (26%letters == 0) 
+  { 
+    row = 26/letters;
+  } 
+  else 
+  { 
+    row = 26/letters + 1; 
+  } 
+ 
+  //resize alphabet to have _letter_ # of rows, and _columns_ # of columns 
+  alphabet.resize ( row ); 
+  
+  for( int r = 0; r < alphabet.size(); r++)
+  {
+    alphabet[r].resize( letters ); 
+  } 
+  
+  char currentLetter = 'A'; 
+  //tranverse through grid and assign values
+  for (int r = 0; r< alphabet.size(); r++)
+  { 
+    for (int c = 0; c < alphabet[r].size(); c++) 
+    {
+      alphabet[r][c] = currentLetter; 
+      currentLetter++; // increment to next letter in ASCII table 
+      if (currentLetter > 'Z' ) 
+      { 
+        currentLetter = 'Z';
+      }
+    } 
+  }
+
+  for (int r = 0; r < alphabet.size(); r++)
+  { 
+    for (int c=0; c < alphabet[r].size(); c++) 
+    { 
+     cout<< alphabet[r][c]; 
+    }
+    cout<< endl;
+  }
 
   return 0;
+
 }
